ac4707b44877916e9ca8f82d2796db0b92b58fcf
Alexey Nesterenko <alexei.ne@gmail.com>
2020-02-16 04:05:32 +0700

    grammar

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
+const Helper = require("./lib/Helper");
+
 const pronouns = require("./data/pronoun");
+const tenses = require("./data/tense");
+const verbs = require("./data/verb");
 
 console.log('singular:', pronouns.filter(obj => obj.grammarNumber === 'singular').map(obj => obj.spelling.subject));
 console.log("plural:", pronouns.filter(obj => obj.grammarNumber === 'plural').map(obj => obj.spelling.subject));
@@ -6,3 +10,11 @@ console.log("plural:", pronouns.filter(obj => obj.grammarNumber === 'plural').ma
 console.log('1st:', pronouns.filter(obj => obj.person === 1).map(obj => obj.spelling.subject));
 console.log("2nd:", pronouns.filter(obj => obj.person === 2).map(obj => obj.spelling.subject));
 console.log("3rd:", pronouns.filter(obj => obj.person === 3).map(obj => obj.spelling.subject));
+
+const firstSentence = Helper.renderSentence({
+    "subject": pronouns[0],
+    "verb": verbs[1],
+    "object": "a doctor"
+}, tenses.present.simple, "affirmative");
+
+console.log("firstSentence:", firstSentence);
---
+++ src/lib/Helper.js
@@ -0,0 +1,7 @@
+function renderSentence(parts, tense, type) {
+    return "[render result]"
+}
+
+module.exports = {
+    renderSentence
+};
