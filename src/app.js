2433f1d52cf59608d75dfbee15e336447e3187ac
Alexey Nesterenko <alexei.ne@gmail.com>
2020-02-15 15:05:56 +0700

    pronouns added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-console.log("english phrase generator");
+const pronouns = require("./data/pronount");
+
+console.log('singular:', pronouns.filter(obj => obj.grammarNumber === 'singular').map(obj => obj.spelling.subject));
+console.log("plural:", pronouns.filter(obj => obj.grammarNumber === 'plural').map(obj => obj.spelling.subject));
