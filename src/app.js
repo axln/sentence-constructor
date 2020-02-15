b5c1e9300880e16a43b86f9196b45ccd7c3e7e74
Alexey Nesterenko <alexei.ne@gmail.com>
2020-02-15 16:06:11 +0700

    aux verbs added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
-const pronouns = require("./data/pronount");
+const pronouns = require("./data/pronoun");
 
 console.log('singular:', pronouns.filter(obj => obj.grammarNumber === 'singular').map(obj => obj.spelling.subject));
 console.log("plural:", pronouns.filter(obj => obj.grammarNumber === 'plural').map(obj => obj.spelling.subject));
+
+console.log('1st:', pronouns.filter(obj => obj.person === 1).map(obj => obj.spelling.subject));
+console.log("2nd:", pronouns.filter(obj => obj.person === 2).map(obj => obj.spelling.subject));
+console.log("3rd:", pronouns.filter(obj => obj.person === 3).map(obj => obj.spelling.subject));
