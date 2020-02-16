bd5985843705e3a8b0e6488e6e468da279f77788
Alexey Nesterenko <alexei.ne@gmail.com>
2020-02-16 11:46:17 +0700

    dev

--- src/app.js
+++ src/app.js
@@ -4,17 +4,56 @@ const pronouns = require("./data/pronoun");
 const tenses = require("./data/tense");
 const verbs = require("./data/verb");
 
-console.log('singular:', pronouns.filter(obj => obj.grammarNumber === 'singular').map(obj => obj.spelling.subject));
-console.log("plural:", pronouns.filter(obj => obj.grammarNumber === 'plural').map(obj => obj.spelling.subject));
 
-console.log('1st:', pronouns.filter(obj => obj.person === 1).map(obj => obj.spelling.subject));
-console.log("2nd:", pronouns.filter(obj => obj.person === 2).map(obj => obj.spelling.subject));
-console.log("3rd:", pronouns.filter(obj => obj.person === 3).map(obj => obj.spelling.subject));
+function fillTenseCombo() {
+    const tenseCombo = document.getElementById('tense');
+    const aspectCombo = document.getElementById('aspect');
 
-const firstSentence = Helper.renderSentence({
-    "subject": pronouns[0],
-    "verb": verbs[1],
-    "object": "a doctor"
-}, tenses.present.simple, "affirmative");
+    tenseCombo.onchange = () => {
+        const option = tenseCombo.options[tenseCombo.selectedIndex];
+        console.log('tenseCombo change:', option.value);
+        const aspects = tenses[option.value];
 
-console.log("firstSentence:", firstSentence);
+        aspectCombo.innerHTML = '';
+        for (let aspect in aspects) {
+            const opt = document.createElement('option');
+            opt.innerHTML = Helper.capitalize(aspect);
+            if (aspects[aspect].alt_name) {
+                opt.innerHTML += ` (${Helper.capitalize(aspects[aspect].alt_name)})`;
+            }
+            opt.value = aspect;
+            aspectCombo.appendChild(opt);
+        }
+    };
+
+    for (let tense in tenses) {
+        const opt = document.createElement('option');
+        opt.innerHTML = Helper.capitalize(tense);
+        opt.value = tense;
+        tenseCombo.appendChild(opt);
+    }
+    tenseCombo.selectedIndex = 1;
+}
+
+window.onload = () => {
+    fillTenseCombo();
+
+    /*
+    console.log('singular:', pronouns.filter(obj => obj.grammarNumber === 'singular').map(obj => obj.spelling.subject));
+    console.log("plural:", pronouns.filter(obj => obj.grammarNumber === 'plural').map(obj => obj.spelling.subject));
+
+    console.log('1st:', pronouns.filter(obj => obj.person === 1).map(obj => obj.spelling.subject));
+    console.log("2nd:", pronouns.filter(obj => obj.person === 2).map(obj => obj.spelling.subject));
+    console.log("3rd:", pronouns.filter(obj => obj.person === 3).map(obj => obj.spelling.subject));
+
+    const firstSentence = Helper.renderSentence({
+        "subject": pronouns[0],
+        "verb": verbs[1],
+        "object": "a doctor"
+    }, tenses.present.simple, "affirmative");
+
+    console.log("firstSentence:", firstSentence);
+
+    */
+
+};
--- src/lib/Helper.js
+++ src/lib/Helper.js
@@ -1,7 +1,8 @@
-function renderSentence(parts, tense, type) {
-    return "[render result]"
-}
-
 module.exports = {
-    renderSentence
+    renderSentence: (parts, tense, type) => {
+        return "[render result]"
+    },
+    capitalize: (string) => {
+        return string.replace('_', ' ').split(' ').map(str => str.charAt(0).toUpperCase() + str.slice(1)).join(' ');
+    }
 };
